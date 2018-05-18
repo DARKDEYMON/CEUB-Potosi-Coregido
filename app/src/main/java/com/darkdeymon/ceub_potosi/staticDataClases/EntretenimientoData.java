593ab153b489c61c0ca805ed8932904993e818c7
Reynaldo Juan Pereira Heredia <darkdeymon04@gmail.com>
2018-05-18 17:03:59 -0400

    v14

--- app/src/main/java/com/darkdeymon/ceub_potosi/staticDataClases/EntretenimientoData.java
+++ app/src/main/java/com/darkdeymon/ceub_potosi/staticDataClases/EntretenimientoData.java
@@ -21,12 +21,11 @@ public class EntretenimientoData {
             entretenimiento.add(new Entretenimiento("Blackmon","Plazuela Tumusla","Discoteca",1231321f,1231231f));
             entretenimiento.add(new Entretenimiento("Killa","Av. Universitaria","Karaoke Discoteca",-19.577764f,-65.76585f));
             entretenimiento.add(new Entretenimiento("Venus","Av. Universitaria","Karaoke Discoteca",-19.583606f,-65.759285f));
-            entretenimiento.add(new Entretenimiento("Alternatico","Av. Arce","Karaoke Discoteca",-19.580501f,-65.755586f));
-            entretenimiento.add(new Entretenimiento("el encanto","chayanta","Karaoke Discoteca",-19.586458f,-65.755183f));
-            entretenimiento.add(new Entretenimiento("valentinos","italia","Karaoke Discoteca",-19.580019f,-65.767261f));
-            entretenimiento.add(new Entretenimiento("babel","murillo","Karaoke Discoteca",-19.576723f,-65.765448f));
-            entretenimiento.add(new Entretenimiento("abnesia","italia","Karaoke Discoteca",-19.580657f,-65.766198f));
-            entretenimiento.add(new Entretenimiento("babilon","saavedra","Discoteca",-19.572967f,-65.759191f));
+            entretenimiento.add(new Entretenimiento("Alternativo","Av. Arce","Karaoke Discoteca",-19.580501f,-65.755586f));
+            entretenimiento.add(new Entretenimiento("El encanto","chayanta","Karaoke Discoteca",-19.586458f,-65.755183f));
+            entretenimiento.add(new Entretenimiento("Babel","Murillo","Karaoke Discoteca",-19.576723f,-65.765448f));
+            entretenimiento.add(new Entretenimiento("Abnesia","Italia","Karaoke Discoteca",-19.580657f,-65.766198f));
+            entretenimiento.add(new Entretenimiento("Babilon","saavedra","Discoteca",-19.572967f,-65.759191f));
 
             llenadoen = false;
         }
